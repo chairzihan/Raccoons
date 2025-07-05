5096de49cd65f4b586378475a06f94910a3dd27f
Angela Zheng <angelazheng2007@gmail.com>
2025-07-05 19:34:38 -0400

    Add customer prefab

---
+++ Tim Hortoons/Assets/Scripts/Customer.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+  public bool isRaccoon = false;
+  public string greeting = "Hello!";
+  public int coffeeOrder = 0;
+  public int donutOrder = 0;
+}
