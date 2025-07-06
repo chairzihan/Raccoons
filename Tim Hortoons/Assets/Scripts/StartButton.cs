ac58834c2fd5d4d4888edf69064e8756ee115d48
Angela Zheng <angelazheng2007@gmail.com>
2025-07-05 23:31:41 -0400

    Add navigation bar

--- Tim Hortoons/Assets/Scripts/StartButton.cs
+++ Tim Hortoons/Assets/Scripts/StartButton.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class StartButton : MonoBehaviour
 {
+
+    public GameObject startMenu;
+    public GameObject tutorial;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void StartGame()
+    {
+        startMenu.SetActive(false);
+        tutorial.SetActive(true);
     }
 }
