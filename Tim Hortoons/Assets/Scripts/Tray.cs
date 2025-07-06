75ac189da6bd56af894ad8ad38b089ba2a4be90f
Angela Zheng <angelazheng2007@gmail.com>
2025-07-05 21:40:16 -0400

    Add donuts and tray

---
+++ Tim Hortoons/Assets/Scripts/Tray.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Tray : MonoBehaviour
+{
+    public bool hasDrink = false;
+    public bool hasDonut = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
